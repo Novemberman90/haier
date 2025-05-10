7947bdcb368ab94630efea82e87caa1dcd7f0cda
Novemberman90 <alexledovit@gmail.com>
2025-05-10 08:30:00 +0300

    add addaptive in multibaner section and add animete from hrader logo

--- js/main.js
+++ js/main.js
@@ -38,27 +38,28 @@ const headerScroll = () => {
 
   if (scrollPosition >= headerHeight + 50  && !isScrolled) {
     isScrolled = true;
-    headerLogo.classList.remove('header__logo--top');
     headerLogo.classList.add('header__logo--scrolled');
-    headerActive.classList.remove('header--active');
-    headerWrapper.classList.remove('header__wrapper--visible');
-    headerInner.classList.remove('header__inner--visible');
-
     setTimeout(() => {
-      headerLogo.classList.remove('header__logo--scrolled');
-    }, 10);
+      headerLogo.classList.remove('header__logo--top');
+      headerActive.classList.remove('header--active');
+      headerWrapper.classList.remove('header__wrapper--visible');
+      headerInner.classList.remove('header__inner--visible');
+      setTimeout(() => {
+        headerLogo.classList.remove('header__logo--scrolled');
+      }, 50);
+    }, 200);
       
   } else if (scrollPosition <= headerHeight + 50  && isScrolled) {
     isScrolled = false;
-    headerActive.classList.add('header--active');
-    headerWrapper.classList.add('header__wrapper--visible');
-    headerInner.classList.add('header__inner--visible');
     headerLogo.classList.add('header__logo--scrolled');
-    headerLogo.classList.add('header__logo--top');
-
     setTimeout(() => {
+      headerActive.classList.add('header--active');
+      headerWrapper.classList.add('header__wrapper--visible');
+      headerInner.classList.add('header__inner--visible');
+      headerLogo.classList.add('header__logo--top');
       headerLogo.classList.remove('header__logo--scrolled');
-    }, 10);
+      
+    }, 200);
   }
 };
 
